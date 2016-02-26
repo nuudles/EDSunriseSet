3012c7a05658f61cde07cddf4809b35303ece7c6
Christopher Luu <nuudles@gmail.com>
2016-02-26 17:25:56 -0500

    Add Carthage support

---
+++ EDSunriseSetSample/EDSunriseSet/EDSunriseSetFramework.h
@@ -0,0 +1,19 @@
+//
+//  EDSunriseSet.h
+//  EDSunriseSet
+//
+//  Created by Christopher Luu on 2/26/16.
+//  Copyright © 2016 cocoawithchurros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EDSunriseSet.
+FOUNDATION_EXPORT double EDSunriseSetVersionNumber;
+
+//! Project version string for EDSunriseSet.
+FOUNDATION_EXPORT const unsigned char EDSunriseSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDSunriseSet/PublicHeader.h>
+
+#import <EDSunriseSet/EDSunriseSet.h>
